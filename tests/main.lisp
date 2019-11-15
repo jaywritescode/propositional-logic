4bbb664f9afd8e06184601a66061f79f4a4ac077
Jay Harris <jaywritescode@users.noreply.github.com>
2019-11-15 17:05:06 -0500

    import tt-entails? into test file, remove pl-true?

--- tests/main.lisp
+++ tests/main.lisp
@@ -2,32 +2,10 @@
 (defpackage propositional-logic/tests/main
   (:use :cl
         :rove)
-  (:import-from :propositional-logic :pl-true?))
+  (:import-from :propositional-logic :tt-entails?))
 (in-package :propositional-logic/tests/main)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :propositional-logic)' in your Lisp.
-
-(deftest test-pl-true?
-  (let ((model '((p . t) (q . nil) (r . t) (s . nil))))
-    (testing "a symbol resolves to its value in the model"
-      (ok (pl-true? 'p model))
-      (ng (pl-true? 'q model)))
-    (testing "not p is the negation of p"
-      (ok (pl-true? '(:not q) model))
-      (ng (pl-true? '(:not p) model)))
-    (testing "p and q is the conjunction of p and q"
-      (ok (pl-true? '(p :and r) model))
-      (ng (pl-true? '(p :and q) model)))
-    (testing "p or q is the disjunction of p and q"
-      (ok (pl-true? '(p :or q) model))
-      (ng (pl-true? '(q :or s) model)))
-    (testing "p implies q is the implication of p concludes q"
-      (ok (pl-true? '(q :implies r) model))
-      (ok (pl-true? '(p :implies r) model))
-      (ng (pl-true? '(p :implies q) model)))
-    (testing "p iff q is the biconditional of p and q"
-      (ok (pl-true? '(q :iff s) model))
-      (ng (pl-true? '(p :iff s) model)))))
 
 (deftest test-tt-entails?
   (let ((kb '((:not p1-1)
